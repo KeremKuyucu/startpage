2b6bd660a64cff8df1b6af94c19f04e9e73e77ca
Kerem Kuyucu <84400545+KeremKuyucu@users.noreply.github.com>
2025-07-14 20:09:42 +0300

    Update userconfig.js

--- userconfig.js
+++ userconfig.js
@@ -43,7 +43,7 @@ const default_configuration = {
   disabled: [],
   localIcons: true,
   localFonts: true,
-  fastlink: "https://www.perplexity.ai",
+  fastlink: "",
   openLastVisitedTab: true,
   tabs: [
     {
